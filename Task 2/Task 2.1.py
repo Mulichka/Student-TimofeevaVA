54e31757ee7eef353cafa0d12696ff9c479bdd57
Mulichka <vera.timofeeva.22@gmail.com>
2021-07-21 15:51:43 +0300

    task 1

---
+++ Task 2/Task 2.1.py
@@ -0,0 +1,7 @@
+#1.	Выяснить тип результата выражений:
+variable_1 = 15 * 3
+variable_2 = 15 / 3
+variable_3 = 15 // 2
+variable_4 = 15 ** 2
+
+print(type(variable_1),type(variable_2), type(variable_3), type(variable_4))
