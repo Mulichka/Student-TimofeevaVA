83ff62f947c9b1339edf79c49867ac8df34983c7
Mulichka <vera.timofeeva.22@gmail.com>
2021-07-22 07:01:38 +0300

    task 2 не до конца решено

--- Task 2/Task 2.2.py
+++ Task 2/Task 2.2.py
@@ -3,14 +3,30 @@
 
 list_1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 list_2 = []
+get_in = '5'
 for i in list_1:
-    if i.isdigit() == True and int(i) // 10 == 0:
-        i = "'" + '0' + i + "'"
+    if get_in in i:
+        list_2.append(i.replace('5', '05'))
+    else:
         list_2.append(i)
-
+list_2 = ' '.join(list_2)
 print(list_2)
-list_2 = ' '.join(list_1)
+# list_2 = []
+#     [x for x,y in enumerate(list_1) if "5" in y]
+       # x = x.zfill(2)
 
-print(list_2)
+
+# for i, el in enumerate(list_1):
+#         if el.isdigit() == True and int(el) // 10 == 0:
+#             a = str(el)
+#             a = el.zfill(2)
+#             list_2.append(a)
+#         else:
+#             list_2.append(el)
+#
+# print(list_2)
+# list_2 = ' '.join(list_2)
+#
+# print(list_2)
 
 #print(f{"  "})
