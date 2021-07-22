b7f90329fa63ecdc2eeccc4318b1543e12d6df26
Mulichka <vera.timofeeva.22@gmail.com>
2021-07-22 07:54:45 +0300

    task 3

--- Task 2/Task 2.3.py
+++ Task 2/Task 2.3.py
@@ -4,15 +4,16 @@ list_1 = ['в', '5', 'часов', '17', 'минут', 'температура',
 
 c = 0
 for i in list_1:
-   # if i.isdigit():
+    # if i.isdigit():
     if i.isnumeric():
         if int(i) // 10 > 0:
             list_1[c] = '"' + list_1[c] + '"'
         else:
             list_1[c] = '"' + '0' + list_1[c] + '"'
-    elif (i.find('+') >= 0):
+    else:
+        (i.find('+') >= 0)
         list_1[c] = list_1[c].replace('+5', '"+05"')
-    c = c + 1
+    c += 1
 print(list_1)
 sentence = " ".join(list_1)
 print(sentence)
