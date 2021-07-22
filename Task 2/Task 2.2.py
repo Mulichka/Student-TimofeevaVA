574713ed23aabae0c211e044a537e91cb89eb646
Mulichka <vera.timofeeva.22@gmail.com>
2021-07-22 07:50:52 +0300

    task 3

--- Task 2/Task 2.2.py
+++ Task 2/Task 2.2.py
@@ -9,20 +9,26 @@ for i in list_1:
         list_2.append(i.replace('5', '05'))
     else:
         list_2.append(i)
+
+for i, el in enumerate(list_1):
+if el.isdigit() == True and int(el) // 10 == 0:
+            a = str(el)
+            a = el.zfill(2)
+            list_2.append(a)
+        else:
+            list_2.append(el)
+
 list_2 = ' '.join(list_2)
 print(list_2)
+
+
 # list_2 = []
 #     [x for x,y in enumerate(list_1) if "5" in y]
        # x = x.zfill(2)
 
 
-# for i, el in enumerate(list_1):
-#         if el.isdigit() == True and int(el) // 10 == 0:
-#             a = str(el)
-#             a = el.zfill(2)
-#             list_2.append(a)
-#         else:
-#             list_2.append(el)
+
+#
 #
 # print(list_2)
 # list_2 = ' '.join(list_2)
