636db3a793c433516c96b5710e2b1b9d6bbe1245
Mulichka <vera.timofeeva.22@gmail.com>
2021-08-08 19:35:38 +0300

    Пропущенные задания

---
+++ Task 4/Test.py
@@ -0,0 +1,11 @@
+from itertools import groupby
+
+things = [("animal", "bear"), ("animal", "duck"), ("plant", "cactus"),
+          ("vehicle", "speed boat"), ("vehicle", "school bus")]
+
+for key, group in groupby(things, lambda x: x[0]):
+    for thing in group:
+        print("A %s is a %s." % (thing[1], key))
+
+
+
